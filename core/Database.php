f3f97660c8e7f8b26c9abfca0b7b326e6f7171a3
Климчук Анна Богданівна <ipz242_kab@student.ztu.edu.ua>
2026-05-07 16:07:17 +0300

    Add database connection and database configuration

--- core/Database.php
+++ core/Database.php
@@ -1,6 +1,28 @@
 <?php
-
 class Database
 {
+    private static ?PDO $instance = null;
 
+    public static function connect(): PDO
+    {
+        if (self::$instance === null) {
+
+            $config = require __DIR__ . '/../config/database.php';
+
+            $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8mb4";
+
+            self::$instance = new PDO(
+                $dsn,
+                $config['username'],
+                $config['password']
+            );
+
+            self::$instance->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+        }
+
+        return self::$instance;
+    }
 }
